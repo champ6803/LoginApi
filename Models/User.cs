056ca904d9270987748dd72d6449dc122b9874dd
champ <champ6803@gmail.com>
2018-07-31 17:40:42 +0700

    Create Project

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GenerateUserApi.Models
+{
+    public class User
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string email { get; set; }
+        public string user { get; set; }
+        public string pwd { get; set; }
+        public string type_account { get; set; }
+    }
+}
